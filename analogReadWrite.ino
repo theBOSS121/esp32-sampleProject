71b495f2174664967bdcf0d0b269447c84186133
Luka Uranic <luka.uranic12@gmail.com>
2019-11-27 16:11:29 +0100

    deleted comments, reorganizing the code

--- analogReadWrite.ino
+++ analogReadWrite.ino
@@ -1,18 +1,13 @@
-// Potentiometer is connected to GPIO 34 (Analog ADC1_CH6) 
 const int potPin = 15;
-
-// variable for storing the potentiometer value
 int potValue = 0;
 
 void setup() {
   Serial.begin(115200);
   delay(1000);
-
   pinMode(2, OUTPUT);
 }
 
 void loop() {
-  // Reading potentiometer value
   potValue = analogRead(potPin);
   Serial.println(potValue);
 //  analogWrite does not work there is a workaround using ledc
